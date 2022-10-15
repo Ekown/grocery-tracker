3aaa329d69ff3cf928d50edffe31a5fa9033be55
Eron Tancioco <etancioco@crmonline.com.au>
2022-10-15 23:42:22 +0800

    Turn back to create-react-app architecture and put Express code inside server folder

--- server/index.js
+++ server/index.js
@@ -10,14 +10,18 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.json({ message: "Hello from server!" });
 });
-  
+
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
-  
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+module.exports = app;
