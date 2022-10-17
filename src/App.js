82d488462353ccc94e60f5a5b9337342806c0f81
Eron Tancioco <etancioco@crmonline.com.au>
2022-10-17 15:10:58 +0800

    Add react-bootstrap and buttons in the Home Page for adding and viewing invoices

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import config from "./config/config";
 import ClipLoader from "react-spinners/ClipLoader";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 
 function App() {
@@ -13,27 +15,31 @@ function App() {
     fetch(`${config.API_URL}/api`)
       .then(res => res.json())
       .then(data => setData(data.message))
-      .then(() => setLoading(false));  
+      .then(() => setLoading(false));
   }, []);
 
   return (
-    <div className="App">
-    { loading ? 
-      <div className="loader-container">
-        <ClipLoader
-          color="limegreen"
-          loading={loading}
-          size={150}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
-      </div>
-       :
-      <div className="main-content">
-        <p>{!data ? "Loading..." : data}</p>
-      </div>
-    }      
-    </div>
+    <Container className="App">
+      {loading ?
+        <div className="loader-container">
+          <ClipLoader
+            color="limegreen"
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+        :
+        <div className="main-content">
+          <p>{!data ? "Loading..." : data}</p>
+          <div>
+            <Button variant="primary">Add Invoice</Button>
+            <Button variant="primary">View Invoice</Button>
+          </div>
+        </div>
+      }
+    </Container>
   );
 }
 export default App;
