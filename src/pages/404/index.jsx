9bf62d9286f3ae200f32997d78444d884dc90799
Eron Tancioco <etancioco@crmonline.com.au>
2022-10-29 10:14:22 +0800

    Add simple 404 not found page with home redirection

---
+++ src/pages/404/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+    Container, Row, Col, Button
+} from 'react-bootstrap';
+
+const NotFoundPage = () => {
+    const errorCode = '404';
+    const errorMsg = 'We are sorry, but the page you requested was not found.';
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <div className="not-found-wrapper">
+                        {/* <img src={logo} alt={config.webTitle} /> */}
+                        <h1>{errorCode}</h1>
+                        <p>{errorMsg}</p>
+                        <Button as={Link} to="home">Back to Home</Button>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default NotFoundPage;
