e7cc6a96f7e0c41dee2a18e42d8daa98a5e612d6
Eron Tancioco <etancioco@crmonline.com.au>
2022-12-03 19:48:06 +0800

    [REACT] Remove console log from the loading slice

--- src/reducers/loadingSlice.js
+++ src/reducers/loadingSlice.js
@@ -7,7 +7,6 @@ export const loadingSlice = createSlice({
     },
     reducers: {
         setLoading: (state, action) => {
-            console.log('setLoading: ', action.payload);
             state.value = action.payload;
         },
     },
