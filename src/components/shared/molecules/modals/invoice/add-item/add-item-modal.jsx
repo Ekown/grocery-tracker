4b6ee933a9fb905dc40916d79f1ee570a4146bf2
Eron Tancioco <etancioco@crmonline.com.au>
2022-12-04 07:58:08 +0800

    [REACT] Change AddItemModal class component to functional component

--- src/components/shared/molecules/modals/invoice/add-item/add-item-modal.jsx
+++ src/components/shared/molecules/modals/invoice/add-item/add-item-modal.jsx
@@ -1,6 +1,6 @@
 import { Button, InputAdornment, TextField, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
-import React from 'react';
+import React, { useState } from 'react';
 import config from "../../../../../../config/config";
 import QuaggaScanner from '../../../../atoms/quagga-scanner/quagga-scanner';
 import Grid2 from '@mui/material/Unstable_Grid2';
@@ -8,95 +8,83 @@ import CustomModal from '../../../../custom-modal/custom-modal';
 import './add-item-modal.scss';
 import ItemCard from '../../../item-card/item-card';
 
-class AddItemModal extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            serial_number: null,
-            product: null,
-        };
-    }
+function AddItemModal(props) {
+    const [serialNumber, setSerialNumber] = useState(null);
+    const [product, setProduct] = useState(null);
 
     /**
      * Search for the product
      * 
      * @param {Event} e - Event object
      */
-    searchProduct = e => {
-        fetch(`${config.API_URL}/api/products/sku/${this.state.serial_number}`)
+    const searchProduct = e => {
+        fetch(`${config.API_URL}/api/products/sku/${serialNumber}`)
             .then(res => res.json())
             .then(product => {
                 if (product && product !== {}) {
-                    this.setState({ product: product });
+                    setProduct(product);
                 } else {
-                    this.setState({ product: null });
+                    setProduct(null);
                 }
             });
     }
 
-    render() {
-        return (
-            <CustomModal className="add-item-modal" {...this.props}>
-                <Typography id="transition-modal-title" variant="h6" component="h2">
-                    Add Invoice Item
-                </Typography>
-                <Stack>
-                    <Grid2>
-                        <TextField
-                            name="serial_number"
-                            size="small"
-                            label="Serial Number"
-                            margin="normal"
-                            onChange={(event) => {
-                                this.setState({
-                                    serial_number: event.target.value
-                                });
-                            }}
-                            value={this.state.serial_number ?? ''}
-                            InputProps={{
-                                endAdornment: <InputAdornment position="end"><QuaggaScanner onDetected={(code) => {
-                                    this.setState({
-                                        serial_number: code,
-                                    });
-                                }} /></InputAdornment>,
-                            }}
-                        />
-                        <center><Button variant="contained" onClick={this.searchProduct}>Search Product</Button></center>
-                    </Grid2>
+    return (
+        <CustomModal className="add-item-modal" {...props}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+                Add Invoice Item
+            </Typography>
+            <Stack>
+                <Grid2>
+                    <TextField
+                        name="serial_number"
+                        size="small"
+                        label="Serial Number"
+                        margin="normal"
+                        onChange={(event) => {
+                            setSerialNumber(event.target.value);
+                        }}
+                        value={serialNumber ?? ''}
+                        InputProps={{
+                            endAdornment: <InputAdornment position="end"><QuaggaScanner onDetected={(code) => {
+                                setSerialNumber(code);
+                            }} /></InputAdornment>,
+                        }}
+                    />
+                    <center><Button variant="contained" onClick={searchProduct}>Search Product</Button></center>
+                </Grid2>
 
-                    {
-                        this.state.serial_number && this.state.product ?
-                            <Grid2>
-                                {
-                                    this.state.product?.id ?
-                                        <div>
-                                            <ItemCard
-                                                item={{
-                                                    image: this.state.product.image_url,
-                                                    id: this.state.product.id,
-                                                    name: this.state.product.Product.name,
-                                                    size: this.state.product.size,
-                                                    quantity: 1,
-                                                    // price: 70.75,
-                                                    // cost: 70.75,
-                                                }}
-                                            />
-                                            <center>
-                                                <Button
-                                                    variant="contained"
-                                                    onClick={e => {
-                                                        this.props.handleAddProduct(this.state.product)
-                                                    }}>Add Product</Button>
-                                            </center>
-                                        </div> : <div>No product found</div>
-                                }
-                            </Grid2> : null
-                    }
-                </Stack>
-            </CustomModal>
-        );
-    }
+                {
+                    serialNumber && product ?
+                        <Grid2>
+                            {
+                                product?.id ?
+                                    <div>
+                                        <ItemCard
+                                            item={{
+                                                image: product.image_url,
+                                                id: product.id,
+                                                name: product.Product.name,
+                                                size: product.size,
+                                                quantity: 1,
+                                                // price: 70.75,
+                                                // cost: 70.75,
+                                            }}
+                                        />
+                                        <center>
+                                            <Button
+                                                variant="contained"
+                                                onClick={e => {
+                                                    props.handleAddProduct(product)
+                                                }}>Add Product</Button>
+                                        </center>
+                                    </div> : <div>No product found</div>
+                            }
+                        </Grid2> : null
+                }
+            </Stack>
+        </CustomModal>
+    );
 }
 
 export default AddItemModal;
