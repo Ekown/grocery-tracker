749bdcbae197d06e212f9f3192088f67d03737b3
Eron Tancioco <etancioco@crmonline.com.au>
2022-12-04 01:36:24 +0800

    [REACT] Change the CustomAutocomplete class component to a functional component

--- src/components/shared/atoms/custom-autocomplete/custom-autocomplete.jsx
+++ src/components/shared/atoms/custom-autocomplete/custom-autocomplete.jsx
@@ -1,32 +1,11 @@
 // React
 import { Autocomplete, CircularProgress, TextField } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-class CustomAutocomplete extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            open: false,
-            loading: false,
-            options: [],
-        };
-    }
-
-    componentDidUpdate(prevProps) {
-        // Reset the autocomplete branch field when the selected store is changed
-        if (prevProps.name === 'branch' && prevProps.store !== this.props.store) {
-            this.setState({ options: [] }, () => {
-                this.handleChange(null, null);
-            });
-        }
-    }
-
-    componentWillUnmount() {
-        this.setState({ options: [] }, () => {
-            this.handleChange(null, null);
-        });
-    }
+function CustomAutocomplete(props) {
+    const [open, setOpen] = useState(false);
+    const [options, setOptions] = useState([]);
+    const loading = open && options.length === 0;
 
     /**
      * Handle field changes
@@ -34,72 +13,74 @@ class CustomAutocomplete extends React.Component {
      * @param {Object|Event} event 
      * @param {Object} value 
      */
-    handleChange(event, value) {
-        this.props.onChange(value);
+    const handleChange = (event, value) => {
+        props.onChange(value);
     }
 
-    /**
-     * Trigger the fetch option function and store the fetched options
-     */
-    async fetchOptions() {
-        const options = await this.props.fetchOptions();
-
-        if (options) {
-            this.setState({ options: options, loading: false });
+    // Fetch the options if the dropdown is opened and has no options yet
+    useEffect(() => {
+        if (!loading) {
+            return undefined;
         }
-    }
 
-    render() {
-        return (
-            <Autocomplete
-                selectOnFocus
-                clearOnBlur
-                handleHomeEndKeys
-                freeSolo
-                value={this.props.value ?? ''}
-                size={this.props.size ?? 'medium'}
-                name={this.props.name}
-                open={this.state.open}
-                options={this.state.options}
-                loading={this.state.loading}
-                isOptionEqualToValue={(option, value) => option.name === value.name}
-                getOptionLabel={(option) => option.name || ''}
-                onChange={(e, v) => this.handleChange(e, v)}
-                onOpen={() => {
-                    this.setState({
-                        open: true,
-                        loading: this.state.options.length === 0
-                    }, () => {
-                        // We should only fetch the options once
-                        if (this.state.options.length === 0) {
-                            this.fetchOptions();
-                        }
-                    });
-                }}
-                onClose={() => {
-                    this.setState({
-                        open: false,
-                        loading: false,
-                    });
-                }}
-                renderInput={(params) =>
-                    <TextField
-                        {...params}
-                        label={this.props.label}
-                        InputProps={{
-                            ...params.InputProps,
-                            endAdornment: (
-                                <React.Fragment>
-                                    {this.state.loading ? <CircularProgress color="inherit" size={20} /> : null}
-                                    {params.InputProps.endAdornment}
-                                </React.Fragment>
-                            ),
-                        }}
-                    />
+        if (open && options.length === 0) {
+            (async () => {
+                const options = await props.fetchOptions();
+
+                if (options) {
+                    setOptions(options);
                 }
-            />
-        );
-    }
+            })();
+        }
+    }, [loading]);
+
+    // Clear the options when the store prop is changed (the selected store is changed)
+    useEffect(() => {
+        if (!props?.store) {
+            return undefined;
+        }
+
+        setOptions([]);
+    }, [props.store]);
+
+    return (
+        <Autocomplete
+            selectOnFocus
+            clearOnBlur
+            handleHomeEndKeys
+            freeSolo
+            value={props.value ?? ''}
+            size={props.size ?? 'medium'}
+            name={props.name}
+            open={open}
+            options={options}
+            loading={loading}
+            isOptionEqualToValue={(option, value) => option.name === value.name}
+            getOptionLabel={(option) => option.name || ''}
+            onChange={(e, v) => handleChange(e, v)}
+            onOpen={() => {
+                setOpen(true);
+            }}
+            onClose={() => {
+                setOpen(false);
+            }}
+            renderInput={(params) =>
+                <TextField
+                    {...params}
+                    label={props.label}
+                    InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <React.Fragment>
+                                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                                {params.InputProps.endAdornment}
+                            </React.Fragment>
+                        ),
+                    }}
+                />
+            }
+        />
+    );
 }
 
 export default CustomAutocomplete;
