cf3766ba07863444a635a29f9e45f5361370e985
Eron Tancioco <tancioco.eron@gmail.com>
2026-07-26 01:36:48 +0800

    add dynamic base path to vite config for development and production

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+    base: mode === "development" ? "/" : "/grocery-tracker/",
     plugins: [react()],
     resolve: {
         alias: {
@@ -18,4 +19,4 @@ export default defineConfig({
             },
         },
     },
-});
+}));
